0c03641cfa85e32b0fad759c38c66be458a4c8b3
松本一輝 <mormorbump@gmail.com>
2018-01-09 14:45:50 +0900

    routes reset

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'tests/index'
-
-  get 'tests/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
