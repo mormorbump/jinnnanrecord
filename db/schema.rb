55c213e2d9554e3442516e7d7ad639cca803b0d1
KouichiSano <snk.kouichi@gmail.com>
2018-01-09 08:39:52 +0000

    layout and logo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109073542) do
+ActiveRecord::Schema.define(version: 20180109081327) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -160,6 +160,15 @@ ActiveRecord::Schema.define(version: 20180109073542) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "nickname"
+    t.string "postal_code"
+    t.string "address"
+    t.string "tel_num"
+    t.string "blacklist_flag"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
