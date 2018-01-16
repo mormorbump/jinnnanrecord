6a66ce2aaa5350468b1ae9682ddc99b10bd2de37
松本一輝 <mormorbump@gmail.com>
2018-01-16 17:10:13 +0900

    rate create

--- app/models/review.rb
+++ app/models/review.rb
@@ -14,4 +14,19 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  def rate_star
+    case rate
+      when 1 then
+         "★☆☆☆☆"
+      when 2 then
+         "★★☆☆☆"
+      when 3 then
+         "★★★☆☆"
+      when 4 then
+         "★★★★☆"
+      when 5 then
+         "★★★★★"
+      end
+  end
 end
